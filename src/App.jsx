f2d237c5f6772d1092fc1acd310e02b9d6af971c
Muhammad-Bilal-Web-Developer <bilal.asad2015@gmail.com>
2025-05-03 13:40:13 +0500

    updated the HeroSection name to resolve the error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/Herosection.jsx";
+import HeroSection from "./components/HeroSection.jsx";
 import FeatureSection from "./components/FeatureSection.jsx";
 import WorkFlow from "./components/WorkFlow.jsx";
 import Price from "./components/Price.jsx";
